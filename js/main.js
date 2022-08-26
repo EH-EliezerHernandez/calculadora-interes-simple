fcf3d445f5be4993ef56ede9490d530608040d73
Eliezer Hernandez <eliezersam93@gmail.com>
2022-08-26 19:07:12 -0400

    Funcionalidades para el funcionamiento
    Gracias al desarrollo de las funcionalidades en el archivo main.js es posible:
    - Calcular los resultados: Se debe suministras necesariamente los 3 datos requeridos para el cálculo del interés simple y el total a pagar. Estos cálculos se realizan automáticamente al tener los 3 datos ingresados.
    - Validación de datos: Si el usuario intenta ingresar valores que no cumplan con los requerimientos para que se realicen los cálculos efectivamente se lanzara una excepción.
    - Selección de porcentajes: El usuario puede escoger, cambiar entre los botones disponibles para seleccionar el porcentaje o utilizar el input custom para ingresar el valor del porcentaje deseado.
    - Habilitar o deshabilitar el botón reset: El botón reset está establecido como inactivo por defecto. Si el usuario ingreso todos los datos correctamente se habilitara el botón reset, pero si omite o remueve algún dato, el botón volverá a su estado de deshabilitado.
    - Reset: Es posible resetear todos los campos y botón de porcentaje seleccionado, haciendo que este último vuelva a su estado por defecto. Tenga en cuenta que el botón de reset una vez seleccionado se deshabilitara hasta que vuelva a ingresar la información correcta a cada campo, como se indicó anteriormente.
    - Mostrar resultados: Los resultados solo se mostrarán una vez que todos los datos se ingresen correctamente.
    
    Estilos CSS
    - Botón reset: Se aplican estilos que indiquen que el botón reset está deshabilitado.
    - Botones de porcentajes: Al botón seleccionado se le aplicarán estilos, para indicar que esta ha sido seleccionado efectivamente.
    
    Funcionalidades fundamentales a desarrollar para futuras versiones:
    - Deshabilitar funcionalmente el botón reset: El botón reset tiene el aspecto de un botón deshabilitado pero funcionalmente continúa operando.
    - Respuesta a los errores del usuario: Se han desarrollado validaciones para arrojar excepciones en caso de que el usuario no haga algo como debería, pero aún falta mostrar un mensaje que le indique el tipo de error y donde lo está cometiendo.

---
+++ js/main.js
@@ -0,0 +1,223 @@
+const $inputCapital = document.querySelector("#inputCapital");
+const $porcentageContainer = document.querySelector("#porcentageContainer");
+const $inputTime = document.querySelector("#inputTime");
+const $inputCustom = document.querySelector("#inputCustom");
+const $btnReset = document.querySelector("#btnReset");
+
+/*------------------addEventListeners-------------------- */
+
+$inputCapital.addEventListener("input", realizarCalculos);
+$porcentageContainer.addEventListener("click", (evt) => {
+  const target = evt.target;
+  const isBtn = target.classList.contains("btn");
+  if (isBtn) {
+    activeBtn(target);
+    $inputCustom.classList.remove("selected");
+    realizarCalculos();
+  }
+});
+
+$inputTime.addEventListener("input", realizarCalculos);
+
+$inputCustom.addEventListener("click", (evt) => {
+  const target = evt.target;
+  removeAllActive();
+  cleanPlaceHolder(target);
+  target.classList.add("selected");
+  realizarCalculos();
+});
+
+$inputCustom.addEventListener("input", (evt) => {
+  const target = evt.target;
+  target.classList.add("selected");
+  realizarCalculos();
+});
+
+$btnReset.addEventListener("click", () => {
+  resetCampos();
+  addDefaultPlaceholder($inputCustom);
+  removeAllActive();
+  addInactiveState($btnReset);
+  resetResults();
+});
+
+/* $inputCustom.addEventListener("blur", (evt) => {
+  const target = evt.target;
+  addPlaceholder(target);
+});
+ */
+
+/* -----------------functions--------------------- */
+
+function resetResults() {
+  const $resultInteres = document.querySelector("#resultInteres");
+  const $resultTotal = document.querySelector("#resultMontoTotal");
+  $resultInteres.innerText = `$0.00`;
+  $resultTotal.innerText = `$0.00`;
+}
+function resetCampos() {
+  $inputCapital.value = "";
+  $inputTime.value = "";
+  $inputCustom.value = "";
+}
+
+function addInactiveState($element) {
+  $element.classList.add("inactive");
+}
+function removeInactiveState($element) {
+  $element.classList.remove("inactive");
+}
+
+function addDefaultPlaceholder($input) {
+  $input.placeholder = "Custom";
+}
+
+function cleanPlaceHolder($input) {
+  $input.placeholder = "";
+}
+
+function removeAllActive() {
+  const $buttons = document.querySelectorAll(".active");
+  $buttons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+}
+
+function activeBtn($btn) {
+  removeAllActive();
+  $btn.classList.add("active");
+}
+function validarIsNumber(num) {
+  return !isNaN(num);
+}
+
+function validarIsPositiveValue(num) {
+  return num > 0;
+}
+
+function validarIsValidData({ name, value }) {
+  const isValidData = true;
+
+  if (!validarIsNumber(value)) {
+    throw new Error(`El ${name} ingresado debe ser un numero`);
+  }
+  if (!validarIsPositiveValue(value)) {
+    throw new Error(`El ${name} no debe representarse con un valor negativo`);
+  }
+  if (value === 0) {
+    throw new Error(`El ${name} no debe ser de 0%`);
+  }
+  return isValidData;
+}
+
+function validarIsCampoEmpity($campo) {
+  return $campo.value === "";
+}
+
+function validarIsSelectedButtom($option) {
+  return Boolean($option);
+}
+
+class Data {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class empityError extends Error {
+  constructor(mensaje) {
+    super(mensaje);
+  }
+}
+
+class noSelectedOptionError extends Error {
+  constructor(mensaje) {
+    super(mensaje);
+  }
+}
+
+function capturarDatos() {
+  const objCapital = new Data("capital");
+  const objPorcent = new Data("porcentaje");
+  const objTime = new Data("tiempo");
+
+  objCapital.value = parseFloat($inputCapital.value);
+  objTime.value = parseFloat($inputTime.value);
+  const $btnPorcent = document.querySelector(".active");
+  const $inputPorcent = document.querySelector(".selected");
+
+  let porcent;
+  let capital;
+  let time;
+
+  if (validarIsCampoEmpity($inputCapital)) {
+    throw new empityError("El campo capital esta vacio");
+  }
+
+  if (validarIsValidData(objCapital)) {
+    capital = objCapital.value;
+  }
+
+  if (validarIsCampoEmpity($inputTime)) {
+    throw new empityError("El campo tiempo esta vacio");
+  }
+
+  if (validarIsValidData(objTime)) {
+    time = objTime.value;
+  }
+
+  if (!validarIsSelectedButtom($btnPorcent) && !$inputPorcent) {
+    throw new noSelectedOptionError("No se ha seleccionado ningun porcentaje");
+  }
+
+  if ($btnPorcent) {
+    objPorcent.value = parseFloat($btnPorcent.value);
+  }
+
+  if ($inputPorcent) {
+    objPorcent.value = parseFloat($inputPorcent.value);
+  }
+
+  if (validarIsValidData(objPorcent)) {
+    porcent = objPorcent.value;
+  }
+  const objData = {
+    porcent,
+    time,
+    capital,
+  };
+  return objData;
+}
+
+function calcularTotalPagar({ capital, interes }) {
+  const totalPagar = capital + interes;
+  return totalPagar;
+}
+
+function calcularInteresSimple({ time, capital, porcent }) {
+  const interes = time * capital * (porcent / 100);
+  return interes;
+}
+
+function renderResult({ interes, totalPagar }) {
+  const $resultInteres = document.querySelector("#resultInteres");
+  const $resultMontoTotal = document.querySelector("#resultMontoTotal");
+  $resultInteres.innerText = `$${interes}`;
+  $resultMontoTotal.innerText = `$${totalPagar}`;
+}
+
+function realizarCalculos() {
+  try {
+    addInactiveState($btnReset);
+    const objData = capturarDatos();
+    removeInactiveState($btnReset);
+    const objResult = {};
+    objResult.interes = calcularInteresSimple(objData);
+    const interes = objResult.interes;
+    objResult.totalPagar = calcularTotalPagar({ ...objData, interes });
+    console.log({ ...objData, interes });
+    renderResult(objResult);
+  } catch (ex) {
+    console.log(ex);
+  }
+}
